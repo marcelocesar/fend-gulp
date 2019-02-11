4f258a8ab1855cd3cd72c6f214b98d4717c964b3
mhjesus <mhjesus@indracompany.com>
2019-02-11 15:08:14 -0200

    add gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 
 gulp.task('default', () => {
     console.log('Hello, Gulp!');
 });
+
+gulp.task('sass', () => {
+    gulp.src('src/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./dist/assets/css'));
+})
