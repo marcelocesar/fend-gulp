070ef7bdd821f835755d707ecafe90a71bcbaa6a
mhjesus <mhjesus@indracompany.com>
2019-02-12 14:16:23 -0200

    add gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', () => {
+
+gulp.task('default', function() {
     console.log('Hello, Gulp!');
 });
 
-gulp.task('sass', () => {
-    gulp.src('src/sass/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./dist/assets/css'));
+gulp.task('sass', function() {
+    gulp.src('app/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./app/css'))
+})
+
+gulp.task('dev', function () {
+    gulp.watch('./app/sass/**/*',['sass']);
 })
